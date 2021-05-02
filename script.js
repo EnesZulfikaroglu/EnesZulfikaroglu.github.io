67534afec6056c62a3075e261a4042ddf06ab07b
EnesZulfikaroglu <enes.zulfikaroglu34@gmail.com>
2021-05-02 21:17:42 +0300

    pictures fixed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function saatGoster(city) {
-    console.log(city);
 	var saat = document.getElementById("saatKutu");
-	saat.innerText = new Date().toLocaleString("en-US", {timeZone: city});
+	saat.innerText = new Date().toLocaleString("en-US", {timeZone: city}).split(",")[1];
     
 }
 
@@ -12,11 +11,12 @@ function onClick(cityID) {
     saatGoster(city);
 }
 
+var city = "Europe/Istanbul";
 
 const Tokyo = document.getElementById("Asia/Tokyo");
 Tokyo.addEventListener('click', function(){ onClick(Tokyo.id); });
 
-const Istanbul = document.getElementById("Europe/istanbul");
+const Istanbul = document.getElementById("Europe/Istanbul");
 Istanbul.addEventListener('click', function(){ onClick(Istanbul.id); });
 
 const NewYork = document.getElementById("America/New_York");
@@ -24,8 +24,6 @@ NewYork.addEventListener('click', function(){ onClick(NewYork.id); });
 
 const London = document.getElementById("Europe/London");
 London.addEventListener('click', function(){ onClick(London.id); });
-
-var city = "America/New_York";
 
 saatGoster(city);
 setInterval("saatGoster(city)", 1000);
