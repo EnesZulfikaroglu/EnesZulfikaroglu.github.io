82078e11dfa3ad5b67f1c3ff9a6e0b51b8221534
EnesZulfikaroglu <enes.zulfikaroglu34@gmail.com>
2021-05-02 18:02:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,13 @@
-function onClick1() {
-
-    const kutu = document.getElementById("about");
-
-    console.log("Merhaba\n")
-    if(kutu.style.display == 'block')
-    {
-        kutu.style.display = 'none'
-        console.log("\n1")
-    }
-    else
-    {
-        kutu.style.display = 'block'
-        console.log("\n2")
-    }
-
-
+function saatGoster() {
+	var saat = document.getElementById("saatKutu");
+	let time = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+    
+    saat.innerText;
+    
 }
 
-function onClick2() {
 
-    const kutu = document.getElementById("idea");
+saatGoster();
+setInterval("saatGoster()", 1000);
 
-    console.log("Merhaba\n")
-    if(kutu.style.display == 'block')
-    {
-        kutu.style.display = 'none'
-        console.log("\n1")
-    }
-    else
-    {
-        kutu.style.display = 'block'
-        console.log("\n2")
-    }
-}
-
-const button1 = document.getElementById("button1");
-button1.addEventListener('click', onClick1);
-
-const button2 = document.getElementById("button2");
-button2.addEventListener('click', onClick2);
+console.log(time);
